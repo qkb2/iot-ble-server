738d24cfd23e968eb67158df73d04c2de563b81e
qkb2 <jakubgr02@gmail.com>
2026-01-07 14:11:19 +0100

    fix defconfig

---
+++ boards/post-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eo pipefail
+
+GENIMAGE_CFG_IN="${BR2_EXTERNAL_IOT_BLE_SERVER_PATH}/boards/genimage.cfg.in"
+GENIMAGE_CFG="${BINARIES_DIR}/genimage.cfg"
+
+FILES=(
+  "Image.gz"
+)
+
+for i in "${BINARIES_DIR}"/*.dtb "${BINARIES_DIR}"/rpi-firmware/*; do
+  FILES+=("${i#${BINARIES_DIR}/}")
+done
+
+BOOT_FILES=$(printf '\\t\\t\\t"%s",\\n' "${FILES[@]}")
+sed "s|#BOOT_FILES#|${BOOT_FILES}|" "${GENIMAGE_CFG_IN}" \
+  >"${GENIMAGE_CFG}"
+
+support/scripts/genimage.sh -c "${GENIMAGE_CFG}"
