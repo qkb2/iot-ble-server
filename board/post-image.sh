057fa87e91a89f26c94e8d4776dff85e4df3e199
Mikołaj Rosowski <m.rosowski1@wp.pl>
2026-01-13 19:35:09 +0100

    config update
    Signed-off-by: Mikołaj Rosowski <m.rosowski1@wp.pl>

--- board/post-image.sh
+++ board/post-image.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -eo pipefail
 
-GENIMAGE_CFG_IN="${BR2_EXTERNAL_IOT_BLE_SERVER_PATH}/boards/genimage.cfg.in"
+GENIMAGE_CFG_IN="${BR2_EXTERNAL_IOT_BLE_SERVER_PATH}/board/genimage.cfg.in"
 GENIMAGE_CFG="${BINARIES_DIR}/genimage.cfg"
 
 FILES=(
@@ -17,3 +17,4 @@ sed "s|#BOOT_FILES#|${BOOT_FILES}|" "${GENIMAGE_CFG_IN}" \
   >"${GENIMAGE_CFG}"
 
 support/scripts/genimage.sh -c "${GENIMAGE_CFG}"
+
